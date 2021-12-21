3a26f2321462da5e147cfcc3bd8db1daf0ac92b2
Joel Takvorian <jtakvori@redhat.com>
2021-12-21 14:24:29 +0100

    tmp

--- web/src/api/routes.ts
+++ web/src/api/routes.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { LokiResponse, ParsedStream, parseStream } from './loki';
 
-const host = '/api/proxy/namespace/network-observability/service/network-observability-plugin:9001';
+const host = '/api/plugins/network-observability-plugin';
 
 export const getFlows = (): Promise<ParsedStream[]> => {
   return axios.get(host + '/api/loki/flows', {}).then(r => {
